b2229d99ded48d184ce6a46499374ff6839563b8
danfertev <danfertev@gmail.com>
2013-05-16 18:42:52 +0400

    toList, toListRecursion, LinkedList

--- src/main/java/ru/anfdenis/jmh/JavaBenchmark.java
+++ src/main/java/ru/anfdenis/jmh/JavaBenchmark.java
@@ -4,8 +4,9 @@ import org.openjdk.jmh.annotations.BenchmarkType;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.Measurement;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Denis Anfertev
@@ -13,17 +14,36 @@ import java.util.List;
  */
 public class JavaBenchmark {
     @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
-    @Measurement(iterations = 5)
-    public void wrapper() {
-        ScalaBenchmark.test();
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    public void testToBuffer() {
+        ScalaBenchmark.testToBuffer();
     }
 
     @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
-    @Measurement(iterations = 5)
-    public void test() {
-        List<Integer> list = new ArrayList<Integer>(1000000);
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    public void testToBufferWhile() {
+        ScalaBenchmark.testToBufferWhile();
+    }
+
+    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    public void testToList() {
+        ScalaBenchmark.testToList();
+    }
+
+    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    public void testToListRecursion() {
+        ScalaBenchmark.testToListRecursion();
+    }
+
+    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    public void testLinkedList() {
+        List<Integer> list = new LinkedList<Integer>();
         for (int i = 0; i < 1000000; i++) {
             list.add(i);
         }
     }
+
 }
--- src/main/scala/ru/anfdenis/jmh/ScalaBenchmark.scala
+++ src/main/scala/ru/anfdenis/jmh/ScalaBenchmark.scala
@@ -6,7 +6,28 @@ package ru.anfdenis.jmh
  * 16.05.13 16:54
  */
 object ScalaBenchmark {
-  def test() {
+  def testToBuffer() {
     (0 to 1000000).toBuffer
   }
+
+  def testToBufferWhile() {
+    var i = 0
+    val ab = new collection.mutable.ArrayBuffer[Int]()
+    while (i < 1000000) {
+      ab += i
+      i += 1
+    }
+  }
+
+  def testToList() {
+    (0 to 1000000).toList
+  }
+
+  def testToListRecursion() {
+    toListRecursion(999999, Nil)
+  }
+
+  def toListRecursion(x: Int, xs: List[Int]): List[Int] =
+    if (x < 0) xs
+    else toListRecursion(x - 1, x :: xs)
 }
