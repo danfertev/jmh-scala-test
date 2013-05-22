39bc57dc80f1b4269a17d8eedbc14b1bf380cb94
danfertev <danfertev@gmail.com>
2013-05-22 10:54:57 +0400

    withFilter.map vs filter.map, clojure vs func, @specialized

--- src/main/java/ru/anfdenis/jmh/JavaBenchmark.java
+++ src/main/java/ru/anfdenis/jmh/JavaBenchmark.java
@@ -4,8 +4,6 @@ import org.openjdk.jmh.annotations.BenchmarkType;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.Measurement;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,37 +11,72 @@ import java.util.concurrent.TimeUnit;
  * 16.05.13 17:05
  */
 public class JavaBenchmark {
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testToBuffer() {
+//        ScalaBenchmark.testToBuffer();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testToBufferWhile() {
+//        ScalaBenchmark.testToBufferWhile();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testToList() {
+//        ScalaBenchmark.testToList();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testToListRecursion() {
+//        ScalaBenchmark.testToListRecursion();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testLinkedList() {
+//        List<Integer> list = new LinkedList<Integer>();
+//        for (int i = 0; i < 1000000; i++) {
+//            list.add(i);
+//        }
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testWithFilterMap() {
+//        ScalaBenchmark.testWithFilterMap();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testFilterMap() {
+//        ScalaBenchmark.testFilterMap();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testFunc() {
+//        ScalaBenchmark.testFunc();
+//    }
+//
+//    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
+//    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+//    public void testClojure() {
+//        ScalaBenchmark.testClojure();
+//    }
+
     @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
     @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    public void testToBuffer() {
-        ScalaBenchmark.testToBuffer();
+    public void testSum() {
+        ScalaBenchmark.testSum();
     }
 
     @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
     @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    public void testToBufferWhile() {
-        ScalaBenchmark.testToBufferWhile();
+    public void testSumVecWithSpect() {
+        ScalaBenchmark.testSumWithSpec();
     }
-
-    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
-    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    public void testToList() {
-        ScalaBenchmark.testToList();
-    }
-
-    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
-    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    public void testToListRecursion() {
-        ScalaBenchmark.testToListRecursion();
-    }
-
-    @GenerateMicroBenchmark(BenchmarkType.AverageTimePerOp)
-    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-    public void testLinkedList() {
-        List<Integer> list = new LinkedList<Integer>();
-        for (int i = 0; i < 1000000; i++) {
-            list.add(i);
-        }
-    }
-
 }
--- src/main/scala/ru/anfdenis/jmh/ScalaBenchmark.scala
+++ src/main/scala/ru/anfdenis/jmh/ScalaBenchmark.scala
@@ -30,4 +30,62 @@ object ScalaBenchmark {
   def toListRecursion(x: Int, xs: List[Int]): List[Int] =
     if (x < 0) xs
     else toListRecursion(x - 1, x :: xs)
+
+  private val xs = (0 to 1000000).toList
+
+  def testWithFilterMap() {
+    xs.withFilter(_ > 500000).map(_ * 2)
+  }
+
+  def testFilterMap() {
+    xs.filter(_ > 500000).map(_ * 2)
+  }
+
+  private def func(v: Int) = (_: Int) < v
+
+  private val x = 500000
+
+  private def clojure = (_: Int) < x
+
+  def testFunc() {
+    xs.filter(func(x))
+  }
+
+  def testClojure() {
+    xs.filter(clojure)
+  }
+
+  private case class Vec[T](v: Array[T]) {
+    def apply(n: Int): T = v(n)
+  }
+
+  private case class VecWithSpec[@specialized(Int) T](v: Array[T]) {
+    def apply(n: Int): T = v(n)
+  }
+
+  private def sum(xs: Vec[Int]) = {
+    var s = 0
+    for (x <- 0 until xs.v.length) {
+      s += xs(x)
+    }
+    s
+  }
+
+  private def sumWithSpec(xs: VecWithSpec[Int]) = {
+    var s = 0
+    for (x <- 0 until xs.v.length) {
+      s += xs(x)
+    }
+    s
+  }
+
+  private val l = 1000000
+
+  def testSum() {
+    sum(Vec[Int]((1 to l).toArray))
+  }
+
+  def testSumWithSpec() {
+    sumWithSpec(VecWithSpec[Int]((1 to l).toArray))
+  }
 }
